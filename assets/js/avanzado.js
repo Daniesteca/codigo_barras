2d92a1a1f7e8316793b2d1d46460249efef747d6
Daniela <nanysmanila@hotmail.com>
2024-08-22 11:33:52 -0500

    agrega estilos  a la funcionalidad, y lista desplegable de camaras disponibles

--- assets/js/avanzado.js
+++ assets/js/avanzado.js
@@ -7,11 +7,61 @@ function lecturaCorrecta(codigoTexto, codigoObjeto) {
   function errorLectura(error) {
     // handle scan failure, usually better to ignore and keep scanning.
     // for example:
-    console.warn(`Code scan error = ${error}`);
+  //   console.warn(`Code scan error = ${error}`);
+   }
+
+
+
+  // This method will trigger user permissions
+Html5Qrcode.getCameras().then(camaras => {
+ 
+
+  if (camaras && camaras.length) {
+    let camaraId = camaras[0].id;
+   
+    let select = document.getElementById("listaCamaras");
+    let html = ` <option value="" selected>Seleccione una camara</option>`;
+
+    camaras.forEach(camara => {
+      // console.log('camara :>> ',camara);
+      html += ` <option value="${camara.id}">${camara.label}</option>`;
+    });
+
+    select.innerHTML = html;
+    // .. use this to start scanning.
+
+
+
   }
-  
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },
-    /* verbose= */ false);
-  html5QrcodeScanner.render(lecturaCorrecta, errorLectura);
+}).catch(err => {
+  // handle err
+});
+
+
+  const camaraSeleccionada =(elemento)=>{
+    // console.log('elemento :>>', elemento);
+    let idCamaraSeleccionada = elemento.value;
+    console.log('idCamaraSeleccionada :>>', idCamaraSeleccionada);
+
+        const html5QrCode = new Html5Qrcode(/* element id */ "reader");
+        html5QrCode.start(
+          idCamaraSeleccionada, 
+          {
+            fps: 10,    // Optional, frame per seconds for qr code scanning
+            qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
+          },
+          (decodedText, decodedResult) => {
+            // do something when code is read
+            console.log('decodedText :>>', decodedText);
+          },
+          (errorMessage) => {
+            // parse error, ignore it.
+          })
+        .catch((err) => {
+          // Start failed, handle it.
+        });
+
+
+
+
+  }
